9a94216b5b166480ec32a9a7935ec58bf81d7d19
Pastorkid <tijanioluwagbengamiracle@gmail.com>
2023-01-11 07:39:41 -0800

    sever set-up sucessfully

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,10 +1,29 @@
 const express=require('express')
+const {MongoClient}=require('mongodb');
 const sessions=require('../data/sessions.json')
 const sessionsRouter=express.Router();
 
 sessionsRouter.route('/')
    .get((req,res)=>{
-    res.render('sessions',{sessions});
+    const url=
+    'mongodb+srv://dbUser:RwvRnkl5b2Tuq7uP@globamantics.3oe6f2a.mongodb.net/?retryWrites=true&w=majority';
+    const dbName='Globamantics';
+
+    (async function mongo(){
+       let client;
+        try{
+            client=await MongoClient.connect(url);
+            console.log('connected to the mongodb');
+
+            const db=client.db(dbName);
+            const sessions=await db.collection('sessions').find().toArray();
+            res.render(sessions,{sessions});
+           }catch (error){
+               console.log(error.stack);
+           }
+           client.close();
+    })();
+
    })
 
    sessionsRouter.route('/:id')
